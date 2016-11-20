90e5c18da610f34af5ab6fbda09c893fbf584733
Kyla <kyla2@kylascomputer.local>
2016-11-20 01:46:43 -0500

    plot points on map

--- CRWL/app/controllers/crawls_controller.rb
+++ CRWL/app/controllers/crawls_controller.rb
@@ -1,13 +1,21 @@
 require 'budget.rb'
+require "csv"
 
 class CrawlsController < ApplicationController
 
 	def create
 			@numbers = params[:num_bars]
 			if (@numbers)
-				@client = GooglePlaces::Client.new('AIzaSyCTLRYRo2wwmM3umd6pbRXIVugXUnx0wwI')
+				@client = GooglePlaces::Client.new('AIzaSyBrQcAP2_ux46TjrnAQZs_1ATYpzae-q0M')
 				@spots = @client.spots_by_query('bars in columbus Ohio')
 				@spots = @spots.first(@numbers.to_i )
+
+				CSV.open("public/file.csv", "wb") do |csv|
+					csv << ["longitude", "latitude", "place"]
+					@spots.each do |spot|
+						csv << [spot.lng, spot.lat, spot.name]
+					end
+				end
 	    else
 	    	@numbers = 1;
 	    end
