f256777325e1599bd20036f675f09563a74b7962
Kyla <kyla2@kylascomputer.local>
2016-11-20 06:06:27 -0500

    update crawls controller

--- CRWL/app/controllers/crawls_controller.rb
+++ CRWL/app/controllers/crawls_controller.rb
@@ -5,10 +5,14 @@ class CrawlsController < ApplicationController
 
 	def create
 			@numbers = params[:num_bars]
+			@crawlname = params[:crawl_name]
+			@crawldate = params[:crawl_date]
 			if (@numbers)
 				@client = GooglePlaces::Client.new('AIzaSyBrQcAP2_ux46TjrnAQZs_1ATYpzae-q0M')
 				@spots = @client.spots_by_query('bars in columbus Ohio')
-				@spots = @spots.first(@numbers.to_i )
+				@spots = @spots.first(@numbers.to_i)
+				
+				# check if bar exists, if it doesn't create a new one
 
 				CSV.open("public/file.csv", "wb") do |csv|
 					csv << ["longitude", "latitude", "place"]
@@ -32,5 +36,14 @@ class CrawlsController < ApplicationController
 	end
 
 	def index
+	end
+	
+	def save
+		params[:spot_ids].each do |id|
+			puts id
+		end
+		
+		@crawl = Crawl.create([{ name: params[:crawl_name], user_id: current_user.id, crawl_date: params[:crawl_date] }])
+		return @crawl
 	end
 end
--- CRWL/config/routes.rb
+++ CRWL/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :bars
   devise_for :users
   root to: 'users#index'
   resources :users
 
 	get  "/crawl/create" => "crawls#create"
 	get  "/crawl/create/:num" => "crawls#create"
+	post "/crawl/save/" => "crawls#save"
 end
